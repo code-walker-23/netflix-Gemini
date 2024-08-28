9ffef17b1d88cc21b58e10da8ed0f0787b526b4a
code-walker-23 <chhabravinay549@gmail.com>
2024-08-28 13:33:10 +0530

    added video_back

--- src/components/FrontPage/VideoBack.js
+++ src/components/FrontPage/VideoBack.js
@@ -7,3 +7,31 @@ const VideoBack = () => {
 }
 
 export default VideoBack
+
+/* import React, { useState } from "react";
+import { getYouTubeTrailerUrl_MUTE } from "../../utils/constant";
+
+const VideoBackground = ({ id }) => {
+  const [trailer, setTrailer] = useState(null);
+
+  const youtubeKey = trailer?.key;
+  const youtubeTrailerUrl = getYouTubeTrailerUrl_MUTE(youtubeKey);
+
+  return (
+    <div className="w-screen">
+      {youtubeKey && (
+        <iframe
+          className="w-screen aspect-video absolute top-0 left-0 z-0 object-cover "
+          src={youtubeTrailerUrl}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      )}
+    </div>
+  );
+};
+
+export default VideoBackground;
+ */
